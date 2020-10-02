477ee30747715079573b41906e032c08f29affad
BunBunBun <fraddy.mail@gmail.com>
2020-10-02 11:43:54 +0500

    Update Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -30,23 +30,23 @@ class Menu extends Phaser.Scene {
         const midX = this.GAME_WIDTH*0.5;
         // console.log('midX', this.cameras.main.centerX);
 
-        // var back = this.add.sprite(this.GAME_WIDTH*0.5, 0, 'bg');
-        // back.setOrigin(0.5, 0);
+        var back = this.add.sprite(this.GAME_WIDTH*0.5, 0, 'bg');
+        back.setOrigin(0.5, 0);
 
-        this.layerMain = this.add.container();
+        // this.layerMain = this.add.container();
 
-        const bg = this.add.image(0, 0, 'bg');
-        bg.setOrigin(0.5, 0);
-        this.layerMain.add(bg);
+        // const bg = this.add.image(0, 0, 'bg').setOrigin(0, 0);
+        // bg.setOrigin(0.5, 0);
+        // this.layerMain.add(bg);
 
-        // const guide = this.add.image(0, 0, 'guide').setOrigin(0, 0).setDepth(1);
-        const guide = this.add.image(0, 0, 'guide').setDepth(1);
-        guide.setOrigin(0.5, 0);
-        this.layerMain.add(guide);
+        const guide = this.add.image(0, 0, 'guide').setOrigin(0, 0);
+        // const guide = this.add.image(0, 0, 'guide').setDepth(1);
+        // guide.setOrigin(0.5, 0);
+        // this.layerMain.add(guide);
 
-        var title = this.add.sprite(0, 480, 'logo');
-        title.setOrigin(0.5);
-        this.layerMain.add(title);
+        var title = this.add.sprite(midX, 300, 'logo');
+        // title.setOrigin(0.5);
+        // this.layerMain.add(title);
 
         title.setInteractive({ useHandCursor: true });
         title.on('pointerup', this.clickStart, this);
@@ -54,12 +54,12 @@ class Menu extends Phaser.Scene {
         this.tweens.add({targets: title, angle: title.angle-2, duration: 1000, ease: 'Sine.easeInOut' });
         this.tweens.add({targets: title, angle: title.angle+4, duration: 2000, ease: 'Sine.easeInOut', yoyo: 1, loop: -1, delay: 1000 });
 
-        this.cameras.main.fadeIn(200);
+        // this.cameras.main.fadeIn(200);
 
         this.title = title;
 
-        this.scoreText = this.add.text(0, 8, 'score: 0', { fontSize: '32px', fill: '#ff0000' });
-        this.layerMain.add(this.scoreText);
+        this.scoreText = this.add.text(50, 8, 'score: 0', { fontSize: '32px', fill: '#ff0000' });
+        // this.layerMain.add(this.scoreText);
 
         //======================================================================
         this.updateCamera();
@@ -72,7 +72,7 @@ class Menu extends Phaser.Scene {
 
     updateCamera ()
     {
-        // const camera = this.cameras.main;
+        const camera = this.cameras.main;
 
         const width = this.scale.gameSize.width;
         const height = this.scale.gameSize.height;
@@ -82,22 +82,25 @@ class Menu extends Phaser.Scene {
         console.log('this.parent.height',this.parent.height,'this.sizer.height',this.sizer.height);
 
         // const x = Math.ceil((this.parent.width - this.sizer.width) * 0.5);
-        // const y = 0;
+        const x = 0;
+        const y = 0;
 
         const scaleX = this.sizer.width / this.GAME_WIDTH;
         const scaleY = this.sizer.height / this.GAME_HEIGHT;
 
         // camera.setViewport(x, y, this.sizer.width, this.sizer.height);
-        // camera.setZoom(Math.max(scaleX, scaleY));
-        // camera.centerOn(this.GAME_WIDTH / 2, this.GAME_HEIGHT / 2);
+        // camera.setViewport(x, y, this.parent.width, this.parent.height);
+        camera.setZoom(Math.max(scaleX, scaleY));
 
         const zoom = Math.max(scaleX, scaleY);
-        const offset = 120 * zoom;
+        const offset = deltaY / zoom;
 
-        this.layerMain.x = width / 2;
-        this.layerMain.y = 0;
+        camera.centerOn(this.GAME_WIDTH / 2, this.GAME_HEIGHT / 2 + offset);
+
+        // this.layerMain.x = width / 2;
+        // this.layerMain.y = 0;
         // this.layerMain.y = (height / 2) - deltaY;
-        this.layerMain.setScale(zoom);
+        // this.layerMain.setScale(zoom);
 
     }
 
